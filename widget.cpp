a61d10e8f565a70d229ea92115db1424cf29c318
khanh3327 <pnkill3327scratch@gmail.com>
2022-04-23 12:21:09 +0700

    Update widget.cpp

--- widget.cpp
+++ widget.cpp
@@ -6,20 +6,15 @@ Widget::Widget(QWidget *parent) :
     ui(new Ui::Widget)
 {
     isTo=true;
-//    setFixedSize(400,300);
     ui->setupUi(this);
-//    connect(ui->convertButton,SIGNAL(clicked()),this,SLOT(convert()));
-//    connect(ui->inputTextBox,SIGNAL(editingFinished()),this,SLOT(convert()));
-//    connect(ui->changeButton,SIGNAL(clicked()),this,SLOT(changeType()));
 }
 void Widget::changeType()
 {
     isTo=!isTo;
     ui->inputLabel->setText((isTo)?"Plain Text:":"ASCII Code:");
     ui->inputTextBox->setPlaceholderText((isTo)?"Try to type somethings...":"Paste or press the ASCII Code here!");
-    ui->inputTextBox->setText(QString());
-//    ui->inputTextBox->
-    ui->outputTextBox->setPlainText(QString());
+    ui->inputTextBox->clear();
+    ui->outputTextBox->clear();
     ui->outputLabel->setText((isTo)?"ASCII Code:":"Plain Text:");
 }
 void Widget::convert()
